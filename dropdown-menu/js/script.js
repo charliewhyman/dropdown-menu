aa335c7cbe597b9173ed33f0d3755f8da152efe4
cwhyman <cwhyma@gmail.com>
2022-02-19 17:40:00 -0500

    Added event listeners to dropdown button

--- dropdown-menu/js/script.js
+++ dropdown-menu/js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+let dropdown = document.getElementById('dropdown');
+let dropdownItems = document.querySelectorAll('.dropdownItem');
+let dropdownArrow = document.getElementById('dropdownArrow');
+
+dropdown.addEventListener('click', () => {
+  if (dropdownArrow.dataset.direction === 'right') {
+    dropdownArrow.dataset.direction = 'down';
+    dropdownArrow.textContent = '\u25bc';
+  } else {
+    dropdownArrow.dataset.direction = 'right';
+    dropdownArrow.textContent = '\u25b6';
+  }
+  dropdownItems.forEach(item => {
+    item.classList.toggle('show');
+  })
+})
